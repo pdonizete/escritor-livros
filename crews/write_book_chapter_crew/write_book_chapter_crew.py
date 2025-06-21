a491c6d7eabbafd4c1a1cfdda6c586098185cb86
pdonizete <paulo@paulosoft.net>
2025-06-21 10:39:36 -0300

    teste

--- crews/write_book_chapter_crew/write_book_chapter_crew.py
+++ crews/write_book_chapter_crew/write_book_chapter_crew.py
@@ -3,7 +3,7 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool
 from langchain_openai import ChatOpenAI
 
-from write_a_book_with_flows.types import Chapter
+from types import Chapter
 
 
 @CrewBase
